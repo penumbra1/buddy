74c263c406375664f8940a4e6b913d983bac2d25
Olga Isakova <penumbral@yandex.ru>
2018-12-30 00:25:50 +0500

    Fixed paths, set up server build with Parcel, SSR works but breaks react-select styles

---
+++ server/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import express from "express";
+import { renderToString } from "react-dom/server";
+import { ServerLocation } from "@reach/router";
+import fs from "fs";
+import path from "path";
+import App from "../src/App";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+fs.readFile(
+  path.join(__dirname, "../client/index.html"),
+  "utf8",
+  (err, html) => {
+    const parts = html.split("Not rendered");
+
+    // Middlewares
+    app.use("/dist", express.static(path.join(__dirname, "../client")));
+    app.use((req, res) => {
+      const jsx = (
+        <ServerLocation url={req.url}>
+          <App />
+        </ServerLocation>
+      );
+      res.send(`${parts[0]}${renderToString(jsx)}${parts[1]}`);
+      res.end();
+    });
+
+    console.log(`Listening on ${port}`);
+    app.listen(port);
+  }
+);
